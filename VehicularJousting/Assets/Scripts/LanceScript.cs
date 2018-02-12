b0512218a9aade20200d74137c836d5c7ab861a7
Kevin <kdalt412@gmail.com>
2018-02-11 22:48:52 -0800

    Feb 12 1:48 (EST) am
    Made the jousting lance and started on collisions

--- VehicularJousting/Assets/Scripts/LanceScript.cs
+++ VehicularJousting/Assets/Scripts/LanceScript.cs
@@ -1,20 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LanceScript : MonoBehaviour {
+public class LanceScript : MonoBehaviour
+{
+    public float speed;
+    Vector3 mousePos;
 
-	public Transform target;
-	Camera cam;
+    void Start()
+    {
+    }
 
-	void Start()
-	{
-		cam = GetComponent<Camera>();
-	}
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-	void Update()
-	{
-		Vector3 screenPos = cam.WorldToScreenPoint(target.position);
-		Debug.Log("target is " + screenPos.x + " pixels from the left");
-	}
+        Quaternion temp = Quaternion.Euler((mousePos.y) * speed, (-1*mousePos.x) * speed, mousePos.z * speed);
+        //Quaternion temp = Quaternion.Euler(new Vector3(transform.rotation.x + (mousePos.y*speed), transform.position.y + (mousePos.x*speed), transform.position.y + (mousePos.z * speed)));
+        transform.SetPositionAndRotation(pos, temp);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Outer"))
+        {
+            Debug.Log("1 point");
+            print("hi");
+        }
+        else if(collision.gameObject.tag.Equals("Inner"))
+        {
+            Debug.Log("5 point");
+        }
+    }
 }
