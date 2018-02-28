64724d65f463643833c764da076dd95521de1aa4
Kevin <kdalt412@gmail.com>
2018-02-28 11:08:26 -0500

    Feb 28th 11:05 (EST) am
    Moved Stuff Around, Canvas Fixing, Other stuff

---
+++ VehicularJousting/Assets/Scripts/GameScenesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScenesController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (GameObject.FindGameObjectsWithTag ("GameController").Length > 1) 
+		{
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- VehicularJousting/Game Prototype/README.txt
+++ VehicularJousting/Game Prototype/README.txt
@@ -4,3 +4,5 @@ Space Bar to Brake
 
 Goal:
 Get 100 points in 5 minutes
+
+Both were built on the respective OS
