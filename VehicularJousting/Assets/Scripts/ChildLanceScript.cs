0eef4dfb0b7ac481cf555ad7953fa49f04438cad
Kevin <kdalt412@gmail.com>
2018-03-23 09:27:22 -0700

    Code Changes

---
+++ VehicularJousting/Assets/Scripts/ChildLanceScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildLanceScript : MonoBehaviour {
+
+    public float speed;
+    float xRot, yRot;
+    Transform car;
+    Vector3 carPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        carPos = GetComponentInParent<Transform>().position;
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+        //xRot = transform.localRotation.eulerAngles.x;
+       // yRot = transform.localRotation.eulerAngles.y;
+        yRot += Input.GetAxis("Mouse X") * speed;
+        xRot += Input.GetAxis("Mouse Y") * speed;
+        Quaternion moveAngle = Quaternion.Euler(xRot, yRot, 0);
+        //Vector3 pos = new Vector3(transform.position.x + transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
+        transform.SetPositionAndRotation(pos, moveAngle);
+    }
+}
--- VehicularJousting/Assets/Scripts/LanceScript.cs
+++ VehicularJousting/Assets/Scripts/LanceScript.cs
@@ -7,9 +7,9 @@ using UnityEngine.SceneManagement;
 
 public class LanceScript : MonoBehaviour
 {
-    public float speed;
-    public GameObject carObj;
-    public Camera cam;
+    //public float speed;
+    //public GameObject carObj;
+    //public Camera cam;
     public float allotedTime = 600;
     public int targetScore = 50;
     float currentScore = 0;
@@ -25,23 +25,23 @@ public class LanceScript : MonoBehaviour
 
     void Update()
     {
-        Vector3 car = carObj.transform.position;
-        Quaternion carQuat = carObj.transform.rotation;
-		//set Pos and Quat equal to the LancePos Game Object
-        Vector3 pos = new Vector3(car.x - .75f, car.y - .35f, car.z + 3.85f);
-        Vector3 mousePos = (Input.mousePosition);
-        Vector3 editMouse = new Vector3(mousePos.x / 100, mousePos.y / 100);
-        Vector3 carAngle = carQuat.eulerAngles;
+  //      Vector3 car = carObj.transform.position;
+  //      Quaternion carQuat = carObj.transform.rotation;
+		////set Pos and Quat equal to the LancePos Game Object
+  //      Vector3 pos = new Vector3(car.x - .75f, car.y - .35f, car.z + 3.85f);
+  //      Vector3 mousePos = (Input.mousePosition);
+  //      Vector3 editMouse = new Vector3(mousePos.x / 100, mousePos.y / 100);
+  //      Vector3 carAngle = carQuat.eulerAngles;
 
-        Quaternion temp = Quaternion.Euler((((-1 * editMouse.y) * speed) + carAngle.x), (((-1 * editMouse.x) * speed) + carAngle.y), ((editMouse.z * speed) + carAngle.z));
+  //      Quaternion temp = Quaternion.Euler((((-1 * editMouse.y) * speed) + carAngle.x), (((-1 * editMouse.x) * speed) + carAngle.y), ((editMouse.z * speed) + carAngle.z));
         
-		//Quaternion temp = Quaternion.Euler(new Vector3(transform.rotation.x + (mousePos.y*speed), transform.position.y + (mousePos.x*speed), transform.position.y + (mousePos.z * speed)));
-        //clamp mouse rotation to be a cone
-		//prevent it from moving around excessively
-		//mouse control affects the lance Position 
-		//Use transform.local position - clamp x and y position and control them with floats
+		////Quaternion temp = Quaternion.Euler(new Vector3(transform.rotation.x + (mousePos.y*speed), transform.position.y + (mousePos.x*speed), transform.position.y + (mousePos.z * speed)));
+  //      //clamp mouse rotation to be a cone
+		////prevent it from moving around excessively
+		////mouse control affects the lance Position 
+		////Use transform.local position - clamp x and y position and control them with floats
 
-		transform.SetPositionAndRotation(pos, temp);
+		//transform.SetPositionAndRotation(pos, temp);
         scoreBoard();
     }
 
